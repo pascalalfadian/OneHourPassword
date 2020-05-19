852f1136d6fd66758f335aff5449dc6c1ffe9714
Pascal Alfadian Nugroho <pascal@dnartworks.co.id>
2020-05-19 11:45:11 +0700

    Remove unused cosmetics. I only need bare HTMLs

--- web/index.php
+++ web/index.php
@@ -1,25 +1,11 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Hello, world!</title>
+</head>
+<body>
+	The quick brown fox jumps over the lazy dog.
+</body>
+</html>
