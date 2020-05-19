e0b439505ed7808d3d9a90d7b756ec4fc4466735
Pascal Alfadian Nugroho <pascal@dnartworks.co.id>
2020-05-19 15:27:26 +0700

    Initial version of public facing interface

--- web/index.php
+++ web/index.php
@@ -1,11 +1,33 @@
-<!doctype html>
+<?php
+date_default_timezone_set('Asia/Jakarta');
+$datetime = new DateTime();
+$plaintext = [
+	'datetime' => $datetime->format('Y-m-d H:00:00'),
+	'key' => ''
+];
+$blank_sha = generate_key($datetime, $plaintext);
+$plaintext['key'] = getenv('password_secret_key');
+$keyed_sha = generate_key($datetime, $plaintext);
+function generate_key($datetime, $plaintext) {
+	return $datetime->format('dH') . '-' . substr(base64_encode(hash('sha256', json_encode($plaintext), TRUE)), 0, 9);
+}
+?><!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Hello, world!</title>
+	<title>One Hour Password</title>
 </head>
 <body>
-	The quick brown fox jumps over the lazy dog.
+	<h1>Welcome!</h1>
+	<h2>tl;dr</h2>
+	<p><strong>Here is your password, copy-and-paste it to your encrypted file: <code><?= htmlspecialchars($keyed_sha) ?></code>.</strong></p>
+	<h2>Additional details</h2>
+	<p>Here is some extra information for debugging details:
+		<ul>
+			<li>datetime: <code><?= htmlspecialchars($plaintext['datetime']) ?></code></li>
+			<li>blank_sha: <code><?= htmlspecialchars($blank_sha) ?></code></li>
+		</ul>
+	</p>
 </body>
 </html>
